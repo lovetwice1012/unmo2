e3110daba947d1a8d033bfd2b996c8b53d885744
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-03-12 13:20:33 +0900

    Update unmo.py

--- unmo/unmo.py
+++ unmo/unmo.py
@@ -38,9 +38,9 @@ class Unmo:
         入力をDictionaryに学習させる。"""
         chance = randrange(0, 100)
         if chance in range(0, 29):
-            self._responder = self._responders['markov']
+            self._responder = self._responders['pattern']
         elif chance in range(30, 49):
-            self._responder = self._responders['markov']
+            self._responder = self._responders['template']
         elif chance in range(50, 69):
             self._responder = self._responders['markov']
         elif chance in range(70, 89):
